1593935ccfd6f04c6649e2c623407c13e6daa4d2
Sunil <sunisimar2002d@gmail.com>
2023-09-28 19:14:32 +0530

    new changes

--- todoServer.js
+++ todoServer.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 //we dont use this todos array bcz we want to store data in file so after restarting our data is saved
 // let todos = [];
@@ -58,9 +62,15 @@ app.delete("/todos/:id", (req, res) => {
   }
 });
 
-app.use((req, res, next) => {
-  res.status(404).send();
+//sending html file when we open localhost:3003
+//this is one way to do this and other is we can use cors package
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
+
+// app.use((req, res, next) => {
+//   res.status(404).send();
+// });
 
 app.listen(3003, () => {
   console.log(`app is listening on port 3003`);
